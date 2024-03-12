4c315d783b277607fe7b9a6a0a230c0ac74f4a48
Matheussmaced <mateusbyakuya3@gmail.com>
2024-03-12 11:19:38 -0300

    feat: deixando mais bonito o params

--- next13-fundamentals/src/app/catalog/product/[...data]/page.tsx
+++ next13-fundamentals/src/app/catalog/product/[...data]/page.tsx
@@ -5,12 +5,14 @@ import { useParams } from 'next/navigation'
 export default function Product() {
   const params = useParams()
 
+  const [id = '', tamanho = '', nome = ''] = Array.from(params.data || [])
+
   return (
     <>
       <div>
-        <p>Id: {params.data[0]}</p>
-        <p>Tamanho: {params.data[1]}</p>
-        <p>Nome: {params.data[2]}</p>
+        <p>Id: {id}</p>
+        <p>Tamanho: {tamanho}</p>
+        <p>Nome: {nome}</p>
       </div>
     </>
   )
