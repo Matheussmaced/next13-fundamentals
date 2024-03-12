63b4fc84da0352c6c5701412cc238e27d871db14
Matheussmaced <mateusbyakuya3@gmail.com>
2024-03-12 20:30:17 -0300

    feat: chamada fetch para APi diretamente do componente

--- next13-fundamentals/src/app/page.tsx
+++ next13-fundamentals/src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+// server components permite que fazamos chamada http ou qualquer outro tipo de operação async diretamente no componente
+// para carregar dados que aquele componente precisa inicialmente para ser exibido
+
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('http://api.github.com/users/Matheussmaced')
+
+  const user = await response.json()
+
+  // só irei fazer esse tipo de carregamento de dados http diretamente do componente quando preciso carregar uma informação
+  // qye precisa está disponivel assim que meu componente for exibido em tela
+
+  // não posso fazer esse tipo de chamada para dados que precisam ser carregados dado a alguma ação do usuario, por exemplo clicar em um botão
+  // para carregar uma informação da API
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <pre> {JSON.stringify(user, null, 2)} </pre>
     </>
   )
 }
