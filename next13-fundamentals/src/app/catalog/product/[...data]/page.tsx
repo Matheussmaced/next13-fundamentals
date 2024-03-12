5cacb932b69448ce2c057a3591d7fdbb3f6838c2
Matheussmaced <mateusbyakuya3@gmail.com>
2024-03-12 20:11:32 -0300

    feat: server components e client components

--- next13-fundamentals/src/app/catalog/product/[...data]/page.tsx
+++ next13-fundamentals/src/app/catalog/product/[...data]/page.tsx
@@ -1,11 +1,24 @@
-'use client'
+'use client' // Quer dizer quir esse componente será hidratado, ou seja, todo js que é nescessario que esse componente seja exibido em tela
+// irá ser enviado para o navegador
 
 import { useParams } from 'next/navigation'
+
+// Server Components => toda rota criada com next é server component, que a gente não usa JavaScript no lado do cliente
+
+// Server Components => A gente não usa JavaScript no lado do cliente (navegador)
+// Client Components => O JavaScript é enviado ao navegador (cliente)
 
 export default function Product() {
   const params = useParams()
 
   const [id = '', tamanho = '', nome = ''] = Array.from(params.data || [])
+  // const [id,tamanho,nome] = params.data
+
+  console.log(params)
+
+  function addToCart() {
+    console.log('adicionou ao carrinho')
+  }
 
   return (
     <>
@@ -13,6 +26,8 @@ export default function Product() {
         <p>Id: {id}</p>
         <p>Tamanho: {tamanho}</p>
         <p>Nome: {nome}</p>
+
+        <button onClick={addToCart}>Adicionar ao carrinho</button>
       </div>
     </>
   )
