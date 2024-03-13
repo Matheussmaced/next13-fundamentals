ee89704721fb7e2f92d0eb3ab3c278ace04edd82
Matheussmaced <mateusbyakuya3@gmail.com>
2024-03-13 12:01:44 -0300

    feat: requisição http em componente use client

--- next13-fundamentals/src/app/catalog/product/[...data]/page.tsx
+++ next13-fundamentals/src/app/catalog/product/[...data]/page.tsx
@@ -2,14 +2,35 @@
 // irá ser enviado para o navegador
 
 import { useParams } from 'next/navigation'
+import { useEffect } from 'react'
 
 // Server Components => toda rota criada com next é server component, que a gente não usa JavaScript no lado do cliente
 
 // Server Components => A gente não usa JavaScript no lado do cliente (navegador)
 // Client Components => O JavaScript é enviado ao navegador (cliente)
 
+// não posso usar Async no componente que tem use client, ou seja que tenho interação pacial com js, devido o botão, algum tipo de evento como (onAlgo) e um estado
+// para requerir, posso utilizar o useEfecct como demostrado abaixo
+
+// se vou ter alguma hiperatividade como um botão, se for possivel isola-lo, é melhor para abstrair o maximo do pode do next com server Components
+
 export default function Product() {
   const params = useParams()
+
+  useEffect(() => {
+    const req = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      const response = await fetch('http://api.github.com/users/Matheussmaced')
+
+      const userName = await response.json()
+      console.log(userName.name)
+    }
+
+    req()
+  }, [])
+
+  console.log()
 
   const [id = '', tamanho = '', nome = ''] = Array.from(params.data || [])
   // const [id,tamanho,nome] = params.data
